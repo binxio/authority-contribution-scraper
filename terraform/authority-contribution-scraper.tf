43a746a3eaf42fecbf07ffd4048c668b1efa612d
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2022-01-07 17:22:50 +0100

    use latest build

--- terraform/authority-contribution-scraper.tf
+++ terraform/authority-contribution-scraper.tf
@@ -7,7 +7,7 @@ resource "google_cloud_run_service" "authority-contribution-scraper" {
       container_concurrency = 1
       service_account_name  = google_service_account.authority-contribution-scraper.email
       containers {
-        image = "eu.gcr.io/binxio-mgmt/authority-contribution-scraper:0.1.3"
+        image = "eu.gcr.io/binxio-mgmt/authority-contribution-scraper:4311e1a"
       }
     }
   }
