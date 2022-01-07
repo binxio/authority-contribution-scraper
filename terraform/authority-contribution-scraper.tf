50c33be17714b68c6791461f5f3c57b9c2761493
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2022-01-07 20:19:41 +0100

    bump to version 0.2.0 of the scraper

--- terraform/authority-contribution-scraper.tf
+++ terraform/authority-contribution-scraper.tf
@@ -7,7 +7,7 @@ resource "google_cloud_run_service" "authority-contribution-scraper" {
       container_concurrency = 1
       service_account_name  = google_service_account.authority-contribution-scraper.email
       containers {
-        image = "eu.gcr.io/binxio-mgmt/authority-contribution-scraper:4e6bc55"
+        image = "eu.gcr.io/binxio-mgmt/authority-contribution-scraper:0.2.0"
       }
     }
   }
