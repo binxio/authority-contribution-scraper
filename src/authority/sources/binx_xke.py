ccb20b48623670f2239a4ad1bc3d80a5c6fc61ce
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2022-01-07 20:17:46 +0100

    ensure that all sessions are appended in order

--- src/authority/sources/binx_xke.py
+++ src/authority/sources/binx_xke.py
@@ -1,16 +1,14 @@
 import logging
-import requests
-import os, re
+import os
+import re
 from datetime import datetime
-from typing import Iterator
+from typing import Iterator, List
 
 import pytz
-
+from google.cloud import firestore
 from authority.contribution import Contribution
 from authority.sink import Sink
 from authority.source import Source
-from typing import List
-from google.cloud import firestore
 
 
 def split_presenters(p: str) -> List[str]:
@@ -26,23 +24,80 @@ def split_presenters(p: str) -> List[str]:
     return result if result else [p]
 
 
+def create_from_document(
+    event, session: firestore.DocumentSnapshot
+) -> Iterator[Contribution]:
+    s = session.to_dict()
+    start_time = s.get("startTime", None)
+    if not start_time:
+        logging.error(
+            "%s - %s - does not have a startTime field",
+            event.id,
+            session.id,
+        )
+        return None
+
+    presenters = s.get("presenter", None)
+    if not presenters:
+        logging.error(
+            "%s - %s - does not have a presenter field",
+            event.id,
+            session.id,
+        )
+        return None
+
+    title = s.get("title", None)
+    if not title:
+        logging.error(
+            "%s - %s - does not have a title field",
+            event.id,
+            session.id,
+        )
+        return None
+
+    url = f"https://xke.xebia.com/event/{event.id}/{session.id}/{s.get('slug','')}"
+
+    for presenter in split_presenters(presenters):
+        yield Contribution(
+            guid=url,
+            title=title,
+            author=presenter,
+            date=start_time,
+            url=url,
+            unit="binx",
+            type="xke",
+        )
+
+
 class BinxXkeSource(Source):
     def __init__(self, sink: Sink):
         super(BinxXkeSource, self).__init__(sink)
         self.count = 0
+        self.name = "firestore"
         self.db = firestore.Client(project="xke-nxt")
 
     def feed(self) -> Iterator[Contribution]:
         self.count = 0
         latest = self.sink.latest_entry(unit="binx", contribution="xke")
+
         logging.info(
             "reading new XKE sessions from firestore since %s",
             latest,
         )
+
         now = datetime.now().astimezone(pytz.utc)
-        events = self.db.collection("events").where("startTime", ">=", latest)
+        events = (
+            self.db.collection("events")
+            .where(
+                "startTime",
+                ">=",
+                latest.replace(hour=0, minute=0, second=0, microsecond=0),
+            )
+            .order_by("startTime")
+        )
 
         for event in events.stream():
+            contributions: List[Contribution] = []
             for session_type in ["sessions-public", "sessions-private"]:
                 for session in (
                     self.db.collection("events")
@@ -51,49 +106,15 @@ class BinxXkeSource(Source):
                     .where("unit.id", "==", "BINX")
                     .stream()
                 ):
-                    s = session.to_dict()
-                    start_time = s.get("startTime", None)
-                    if not start_time:
-                        logging.error(
-                            "%s - %s - does not have a startTime field",
-                            event.id,
-                            session.id,
-                        )
-                        continue
+                    for contribution in create_from_document(event, session):
+                        contributions.append(contribution)
 
-                    presenters = s.get("presenter", None)
-                    if not presenters:
-                        logging.error(
-                            "%s - %s - does not have a presenter field",
-                            event.id,
-                            session.id,
-                        )
-                        continue
-                    title = s.get("title", None)
-                    if not title:
-                        logging.error(
-                            "%s - %s - does not have a title field",
-                            event.id,
-                            session.id,
-                        )
-                        continue
+            contributions.sort(key=lambda x: x.date)
 
-                    url = f"https://xke.xebia.com/event/{event.id}/{session.id}/{s.get('slug','')}"
-
-                    if latest < start_time < now:
-                        for presenter in split_presenters(presenters):
-                            contribution = Contribution(
-                                guid=url,
-                                title=title,
-                                author=presenter,
-                                date=start_time,
-                                url=url,
-                                unit="binx",
-                                type="xke",
-                            )
-
-                            self.count = self.count + 1
-                            yield contribution
+            for contribution in contributions:
+                if latest < contribution.date < now:
+                    self.count = self.count + 1
+                    yield contribution
 
 
 if __name__ == "__main__":
