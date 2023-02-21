a41f51a897ce6c74bab02de67f3cb2c527a22300
cru <cru@binx.io>
2023-02-21 10:13:48 +0000

    release eu.gcr.io/binxio-mgmt/authority-contribution-scraper:0.7.1

--- terraform/authority-contribution-scraper.tf
+++ terraform/authority-contribution-scraper.tf
@@ -12,7 +12,7 @@ resource "google_cloud_run_service" "authority-contribution-scraper" {
       container_concurrency = 1
       service_account_name  = google_service_account.authority-contribution-scraper.email
       containers {
-        image = "eu.gcr.io/binxio-mgmt/authority-contribution-scraper:0.7.0"
+        image = "eu.gcr.io/binxio-mgmt/authority-contribution-scraper:0.7.1"
       }
     }
   }
