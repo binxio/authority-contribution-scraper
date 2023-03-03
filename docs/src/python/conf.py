d43dba4db12b2b6049928ce0937ff68811a68ab0
kvanzuijlen <8818390+kvanzuijlen@users.noreply.github.com>
2023-03-03 17:44:27 +0100

    Added sphinx-autoapi

--- docs/src/python/conf.py
+++ docs/src/python/conf.py
@@ -21,7 +21,33 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx_rtd_theme',
+    'autoapi.extension',
 ]
+
+autoapi_type = 'python'
+autoapi_dirs = ['../../../src']
+autoapi_python_class_content = 'both'
+# autoapi_add_toctree_entry = False
+autoapi_options = [
+    'members',
+    'undoc-members',
+    'show-inheritance',
+    'show-inheritance-diagram',
+    'inherited-members',
+    # 'show-module-summary',
+    # 'special-members',
+]
+
+
+def skip_module_attributes(app, what, name, obj, skip, options):
+    if what == "data":
+        skip = True
+    return skip
+
+
+def setup(sphinx):
+   sphinx.connect("autoapi-skip-member", skip_module_attributes)
+
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3', None),
@@ -30,8 +56,6 @@ intersphinx_mapping = {
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
