958a0dd8c5a6de6dc61fe4abc8f430940de2349c
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2024-09-15 12:14:31 +0200

    feat: reorder sources in the load process

--- src/authority/sources/__init__.py
+++ src/authority/sources/__init__.py
@@ -2,8 +2,8 @@
 Module containing all authority contribution sources.
 Imports all sources to automatically register them to the factory.
 """
+from authority.sources.youtube import YoutubeChannel
 from authority.sources.blog import BlogSource
-from authority.sources.github_pull_requests import GithubPullRequests
 from authority.sources.xke import XkeSource
 from authority.sources.article import AuthoritySource
-from authority.sources.youtube import YoutubeChannel
+from authority.sources.github_pull_requests import GithubPullRequests
