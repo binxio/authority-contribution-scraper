c7f63d861cbbcda715471891b8c0db7e4d88e9dd
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2024-11-02 10:36:03 +0100

    feat: allow test source to specify a start date

--- src/authority/util/test_source.py
+++ src/authority/util/test_source.py
@@ -1,24 +1,28 @@
 """
 Module containing the reusable entrypoint code for testing authority sources
 """
+
 import csv
 import dataclasses
 import logging
 import os
-import typing
+
 from datetime import datetime
 from pathlib import Path
 
 import pytz
 
+
 from authority.model.contribution import Contribution
 from authority.sink import Sink
 
-if typing.TYPE_CHECKING:
-    from authority.sources.base_ import AuthoritySource
+from authority.sources.base_ import AuthoritySource
 
 
-def test_source(source: type["AuthoritySource"]) -> "AuthoritySource":
+def test_source(
+    source: type["AuthoritySource"],
+    latest_entry: datetime = datetime.fromordinal(1).replace(tzinfo=pytz.utc),
+) -> "AuthoritySource":
     """
     Method to test sources from their respective entry points
 
@@ -32,9 +36,7 @@ def test_source(source: type["AuthoritySource"]) -> "AuthoritySource":
         level=os.getenv("LOG_LEVEL", "INFO"), format="%(levelname)s: %(message)s"
     )
     sink_ = Sink()
-    sink_.latest_entry = lambda **kwargs: datetime.fromordinal(1).replace(
-        tzinfo=pytz.utc
-    )
+    sink_.latest_entry = lambda **kwargs: latest_entry
     src = source(sink=sink_)
     with Path("./xke_output.csv").open(mode="w", encoding="UTF-8") as file:
         fieldnames = tuple(field.name for field in dataclasses.fields(Contribution))
